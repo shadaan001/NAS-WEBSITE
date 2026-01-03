0b161d23b4a1db228b81ed2f736d2ebc55cbaa34
deepcu-it <deepghosal445@gmail.com>
2026-01-03 22:12:21 +0530

    remove the api keys

--- nas-revolution-centr-main/src/lib/supabase.ts
+++ nas-revolution-centr-main/src/lib/supabase.ts
@@ -1,8 +1,8 @@
 import { createClient } from "@supabase/supabase-js"
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL || "https://uxwnalprswtyngxpsbez.supabase.co"
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY || "sb_publishable_Ut6cQr-rObg_1ZcPCVbErA_erTH8VoQ"
-export const VITE_GOOGLE_API_KEY = import.meta.env.VITE_GOOGLE_API_KEY || "AIzaSyAawR_xLESFOmRrhlqSdqFIWRz2v-yjFpc"
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY 
+export const VITE_GOOGLE_API_KEY = import.meta.env.VITE_GOOGLE_API_KEY
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
 
